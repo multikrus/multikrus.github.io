13c3846f2c36b895d9b3e273fb250fc9171fccbc
Milyutin Sergey <mcm@mail.ru>
2023-05-15 03:45:47 +0300

    feat: автоматическое закрытие бургер-меню при переключении с мобильной на десктопную вёрстку

--- src/blocks/header/__burger/header__burger.js
+++ src/blocks/header/__burger/header__burger.js
@@ -4,26 +4,37 @@ export default function addListener2BurgerIcon() {
     const burgerIcon = document.querySelector(".header__burger");
     const burgerMenu = document.querySelector(".burger-menu");
     const header = document.querySelector(".header");
+    const headerMenu = header.querySelector(".header__menu");
     const html = document.querySelector("html");
 
-    if (!(burgerIcon || burgerMenu || header)) {
+    if (!(burgerIcon || burgerMenu || header || headerMenu)) {
         return;
+    }
+
+    // Теоретически возможен такой кейс: у пользователя в портретном режиме отображается мобильная версия и он
+    // в ней открыл бургер-меню. Потом экран сменился на альбомный и вёрстка стала десктопной, но бургер-меню
+    // осталось висеть, что выглядит коряво, поэтому в подобном кейсе закрываем его
+    const windowResizeHandler = function () {
+        const isMobileHeader = getComputedStyle(headerMenu).display === "none";
+        if (!isMobileHeader) {
+            const {headerHeight, burgerMenuHeight} = getHeights(html, burgerMenu);
+            closeBurgerMenu(header, burgerMenu, headerHeight, burgerMenuHeight);
+            window.removeEventListener("resize", windowResizeHandler);
+        }
     }
 
     burgerIcon.addEventListener("click", () => {
         // Т.к. значения css переменных могут измениться в ходе работы с сайтом, то обращения к ним целесообразно
         // проводить в момент клика по иконке
-        const headerHeight = parseInt(getComputedStyle(html).getPropertyValue("--header-height"));
-        if (!headerHeight) {
-            return;
-        }
+        const {headerHeight, burgerMenuHeight} = getHeights(html, burgerMenu);
 
-        const burgerMenuHeight = Math.round(burgerMenu.getBoundingClientRect().height);
         const isBurgerMenuOpened = header.classList.contains(burgerClass4Header);
         if (isBurgerMenuOpened) {
             closeBurgerMenu(header, burgerMenu, headerHeight, burgerMenuHeight);
+            window.removeEventListener("resize", windowResizeHandler);
         } else {
             openBurgerMenu(header, burgerMenu, headerHeight, burgerMenuHeight);
+            window.addEventListener("resize", windowResizeHandler);
         }
     });
 }
@@ -31,7 +42,7 @@ export default function addListener2BurgerIcon() {
 function openBurgerMenu(header, burgerMenu, headerHeight, burgerMenuHeight) {
     header.classList.add(burgerClass4Header);
     burgerMenu.style.top = `${headerHeight - burgerMenuHeight}px`;
-    window.requestAnimationFrame(() => {
+    requestAnimationFrame(() => {
         burgerMenu.style.opacity = "1";
         burgerMenu.style.top = `${headerHeight + 1}px`;
     });
@@ -41,4 +52,11 @@ function closeBurgerMenu(header, burgerMenu, headerHeight, burgerMenuHeight) {
     header.classList.remove(burgerClass4Header);
     burgerMenu.style.opacity = "0";
     burgerMenu.style.top = `${headerHeight - burgerMenuHeight}px`
+}
+
+function getHeights(html, burgerMenu) {
+    return {
+        "headerHeight": parseInt(getComputedStyle(html).getPropertyValue("--header-height")),
+        "burgerMenuHeight": Math.round(burgerMenu.getBoundingClientRect().height),
+    };
 }
